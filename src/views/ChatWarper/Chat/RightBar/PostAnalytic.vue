c485ad32012fd0a6eb759da8b5eca2d475544b70
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-14 18:08:40 +0700

    update

--- src/views/ChatWarper/Chat/RightBar/PostAnalytic.vue
+++ src/views/ChatWarper/Chat/RightBar/PostAnalytic.vue
@@ -157,12 +157,18 @@
       </li>
       <li>
         {{ $t('Ngày tạo') }}:
-        {{
-          $date_handle.format(
-            conversationStore.select_conversation_post?.content?.created_time,
-            'dd/MM/yyyy'
-          )
-        }}
+        <span
+          v-if="
+            conversationStore.select_conversation_post?.content?.created_time
+          "
+        >
+          {{
+            $date_handle.format(
+              conversationStore.select_conversation_post?.content?.created_time,
+              'dd/MM/yyyy'
+            )
+          }}
+        </span>
       </li>
       <li>
         {{ $t('Người tạo') }}:
@@ -170,12 +176,18 @@
       </li>
       <li>
         {{ $t('Ngày cập nhật') }}:
-        {{
-          $date_handle.format(
-            conversationStore.select_conversation_post?.content?.updated_time,
-            'dd/MM/yyyy'
-          )
-        }}
+        <span
+          v-if="
+            conversationStore.select_conversation_post?.content?.updated_time
+          "
+        >
+          {{
+            $date_handle.format(
+              conversationStore.select_conversation_post?.content?.updated_time,
+              'dd/MM/yyyy'
+            )
+          }}
+        </span>
       </li>
 
       <!-- <li>
